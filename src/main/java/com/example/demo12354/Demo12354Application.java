8534f0cf4011550a2e663aad529b6b316f8ff4e0
Hoang Pham <phamthaihoang1412@gmail.com>
2022-02-12 08:53:04 +0700

    first

---
+++ src/main/java/com/example/demo12354/Demo12354Application.java
@@ -0,0 +1,13 @@
+package com.example.demo12354;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo12354Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo12354Application.class, args);
+    }
+
+}
